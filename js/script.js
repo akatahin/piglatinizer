07b7919abed696418f581956cab7549c02157872
Nahid Kalam <nkalam12@outlook.com>
2019-12-16 22:35:13 +0000

    added code for pig latin project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("document").ready(function() {
 
+$("button").click(function() {
+    let inWord = $("input").val();
+    console.log(inWord);
+    let word = inWord.toLowerCase();
+    console.log(word);
+    let type = word.charAt();
+    console.log(type);
+    var array = word.split(" ");
+    //for ()
+    if ((type === "a") || (type === "e") || (type === "i") || (type === "o") || (type === "u")){
+        console.log("Vowel");
+        
 
+    }else{
+        console.log("constat")
+        let len = word.length;
+        console.log(len);
 
-  
+        let app = word.slice(1 , len);
+        console.log("slice", app);
+        $(".output").text(app + type + "ay");
+
+    }
+
+var aa = "apple orjojf";
+var tt = aa.split(" ");
+console.log(tt);
+// console.log(aa.slice(3));  
   
   
   
 });
+
+});
+
+//python3 -m http.server 3000
